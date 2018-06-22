d35e4817d9cf3a1ccd6bea86cb3ecc1e11528e00
Nowimps8 <31818901+Nowimps8@users.noreply.github.com>
2018-06-21 22:55:11 -1000

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -17,7 +17,7 @@ AddEventHandler('esx_brinks:GiveItem', function()
     local amount = Config.Zones.Vente.ItemAdd
     local item = Config.Zones.Vente.ItemDb_name
     xPlayer.addInventoryItem(item, amount)
-    TriggerClientEvent('esx:showNotification', _source, 'Vous avez vidé ~g~x' .. amount .. ' DAB')
+    TriggerClientEvent('esx:showNotification', _source, 'Collection Point Serviced')
   end
 
 end)
@@ -34,14 +34,14 @@ local function Vente(source)
     local Quantity = xPlayer.getInventoryItem(Config.Zones.Vente.ItemRequires).count
 
     if Quantity < Config.Zones.Vente.ItemRemove then
-      TriggerClientEvent('esx:showNotification', _source, '~r~Vous n\'avez plus de '..Config.Zones.Vente.ItemRequires_name..' à déposer.')
+      TriggerClientEvent('esx:showNotification', _source, '~r~You have no more '..Config.Zones.Vente.ItemRequires_name..' to deposit.')
       PlayersVente[_source] = false
     else
       local amount = Config.Zones.Vente.ItemRemove
       local item = Config.Zones.Vente.ItemRequires
       xPlayer.removeInventoryItem(item, amount)
       xPlayer.addMoney(Config.Zones.Vente.ItemPrice)
-      TriggerClientEvent('esx:showNotification', _source, 'Vous avez reçu ~g~$' .. Config.Zones.Vente.ItemPrice)
+      TriggerClientEvent('esx:showNotification', _source, 'You received ~g~$' .. Config.Zones.Vente.ItemPrice)
       Vente(_source)
     end
 
@@ -55,11 +55,11 @@ AddEventHandler('esx_brinks:startVente', function()
 local _source = source
 
 if PlayersVente[_source] == false then
-  TriggerClientEvent('esx:showNotification', _source, '~r~Sortez et revenez dans la zone !')
+  TriggerClientEvent('esx:showNotification', _source, '~r~Exit and return to the area !')
   PlayersVente[_source] = false
 else
   PlayersVente[_source] = true
-  TriggerClientEvent('esx:showNotification', _source, '~g~Action ~w~en cours...')
+  TriggerClientEvent('esx:showNotification', _source, '~g~Action ~w~in progress...')
   Vente(_source)
 end
 end)
